ef3d2ae46bdb6815ed0bb226a9396989de398f13
Silviu Grijincu <silviu.grijincu@gmail.com>
2011-10-12 00:43:41 +0300

    added NTHBIT

--- bits.c
+++ bits.c
@@ -1,28 +1,32 @@
 #include <stdio.h>
 
 #define MAXBITS 31
+#define NTHBIT(x, n) ((x) & (1 << n))
 
-int nrbits(char *a, char *b, int n)
+int nrbits(int a, int b, int n)
 {
-	if (a[n] == 0 && 0 == b[n])
+	if (n == 0)
+		return 0;
+	
+	if (NTHBIT(a, n) == 0 && NTHBIT(b, n) == 0)
 		return nrbits(a, b, n - 1);
 	
-	if (a[n] == 0 && 1 == b[n])
+	if (NTHBIT(a, n) == 0 && 0 != NTHBIT(b, n))
 	{
 		int y = (1 << n);
 		int x = y - 1;
-		return nrbits(a, (char*)&x, n) + nrbits(b, (char*)&y, n);
+		return nrbits(a, x, n) + nrbits(b, y, n);
 	}
 
-	if (a[n] == 1 && 1 == b[n])
-		return ((int)b - (int)a + 1) + nrbits(a, b, n - 1);
+	if (NTHBIT(a, n) != 0 && 0 != NTHBIT(b, n))
+		return (b - a + 1) + nrbits(a, b, n - 1);
 }
 
 int main()
 {
 	int a = 1;
 	int b = 3;
-	int ret = nrbits((char*)&a, (char*)&b, MAXBITS);
+	int ret = nrbits(a, b, MAXBITS);
 	printf("nr 1's = %d\n", ret);
     return 0;
 }
